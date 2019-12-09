68cfb0c5b0f73eda1c6158d3b51d42d13b5b6b6e
Divine Threepwood <divine@openbase.org>
2019-12-09 17:54:33 +0100

    fix record light control timeout

--- src/main/java/de/citec/csra/studycontrol/RecordLight.java
+++ src/main/java/de/citec/csra/studycontrol/RecordLight.java
@@ -26,16 +26,16 @@ public class RecordLight {
 
     private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(RecordLight.class);
 
-    public static final HSBColorType.HSBColor HSV_COLOR_RED = HSBColorType.HSBColor.newBuilder().setHue(0).setSaturation(100).setBrightness(100).build();
-    public static final HSBColorType.HSBColor HSV_COLOR_PURPLE = HSBColorType.HSBColor.newBuilder().setHue(280).setSaturation(100).setBrightness(100).build();
-    public static final HSBColorType.HSBColor HSV_COLOR_BLUE = HSBColorType.HSBColor.newBuilder().setHue(260).setSaturation(100).setBrightness(100).build();
-    public static final HSBColorType.HSBColor HSV_COLOR_ORANGE = HSBColorType.HSBColor.newBuilder().setHue(60).setSaturation(100).setBrightness(100).build();
-    public static final HSBColorType.HSBColor HSV_COLOR_YELLOW = HSBColorType.HSBColor.newBuilder().setHue(100).setSaturation(100).setBrightness(100).build();
+    public static final HSBColorType.HSBColor HSV_COLOR_RED = HSBColorType.HSBColor.newBuilder().setHue(0).setSaturation(1d).setBrightness(1d).build();
+    public static final HSBColorType.HSBColor HSV_COLOR_PURPLE = HSBColorType.HSBColor.newBuilder().setHue(280).setSaturation(1d).setBrightness(1d).build();
+    public static final HSBColorType.HSBColor HSV_COLOR_BLUE = HSBColorType.HSBColor.newBuilder().setHue(260).setSaturation(1d).setBrightness(1d).build();
+    public static final HSBColorType.HSBColor HSV_COLOR_ORANGE = HSBColorType.HSBColor.newBuilder().setHue(60).setSaturation(1d).setBrightness(1d).build();
+    public static final HSBColorType.HSBColor HSV_COLOR_YELLOW = HSBColorType.HSBColor.newBuilder().setHue(100).setSaturation(1d).setBrightness(1d).build();
 
     public static final String RECORD_LIGHT_ID = "f1397800-9741-401d-a46f-8bf139c12e92";
 
     public static final long RECORD_LIGHT_INIT_TIMEOUT = 3000;
-    public static final long RECORD_LIGHT_TIMEOUT = 100;
+    public static final long RECORD_LIGHT_TIMEOUT = 3000;
 
     public static boolean init() {
         try {
